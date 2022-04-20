a8caef4462b1e4b551bfc1b31b11dd104b945b8d
Joel Stevick <joelstevick@outlook.com>
2022-04-20 14:27:46 -0400

    game logic completed

--- Rock Paper Sizers/ViewController.swift
+++ Rock Paper Sizers/ViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+enum Tag: Int {
+    case paper = 1, rock, scissors
+}
+enum Outcome {
+    case win, lose, draw
+}
+
+struct Challenge: Hashable {
+    var player: Tag
+    var computer: Tag
+}
+let outcome: [Challenge: Outcome] = [
+    Challenge(player: .paper, computer: .paper): .draw,
+    Challenge(player: .paper, computer: .rock): .win,
+    Challenge(player: .paper, computer: .scissors): .lose,
+    Challenge(player: .rock, computer: .paper): .lose,
+    Challenge(player: .rock, computer: .rock): .draw,
+    Challenge(player: .rock, computer: .scissors): .win,
+    Challenge(player: .scissors, computer: .paper): .win,
+    Challenge(player: .scissors, computer: .rock): .lose,
+    Challenge(player: .scissors, computer: .scissors): .draw
+]
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +36,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.tag)
+        
+        let computer = Tag(rawValue: (Int(arc4random()) % 3) + 1)!
+        
+        let player =  Tag(rawValue: sender.tag)!
+        
+        let gameOutcome = outcome[Challenge(player: player, computer: computer)]!
+        
+        print(player, computer, gameOutcome)
+    }
 
 }
 
